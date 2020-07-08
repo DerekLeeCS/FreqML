bad6668c75b7ad96dd90bf78fe25c580556ed04b
Derek Lee <DerekLeeCS@users.noreply.github.com>
2020-07-08 19:47:24 -0400

    added randomization to data separation

--- splitData.cpp
+++ splitData.cpp
@@ -1,17 +1,21 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::cerr;
 using std::endl;
+using std::ifstream;
+using std::ofstream;
 
 #define PERCENT 10
 
 template <class T>
 void checkValidStream(T &,string);
+void addToFile(ofstream &,string,int &,int,bool &);
 
 int main()
 {
@@ -19,34 +23,36 @@ int main()
 
     cout << "Enter the name of the Input file: ";
     cin >> inFile;
-    std::ifstream input(inFile);
+    ifstream input(inFile);
     checkValidStream(input,inFile);
 
 
     cout << "Enter the name of the Training file: ";
     cin >> trainFile;
-    std::ofstream outputTrain(trainFile);
+    ofstream outputTrain(trainFile);
     checkValidStream(outputTrain,trainFile);
 
 
     cout << "Enter the name of the Validation file: ";
     cin >> validFile;
-    std::ofstream outputValid(validFile);
+    ofstream outputValid(validFile);
     checkValidStream(outputValid,validFile);
 
 
     cout << "Enter the name of the Test file: ";
     cin >> testFile;
-    std::ofstream outputTest(testFile);
+    ofstream outputTest(testFile);
     checkValidStream(outputTest,testFile);
 
     string line;
     int lineCount=0;
 
+    // Counts number of lines
     while(input)
     {
         std::getline(input,line);
-        lineCount++;
+        if(line.length() > 0)
+            lineCount++;
     }
 
     // Returns to beginning of file
@@ -58,29 +64,53 @@ int main()
     int validNum = testNum;
 
     // Number of data for Train set
-    int trainNum = lineCount - (validNum + trainNum);
+    int trainNum = lineCount - (validNum + testNum);
 
     // Count of number of data currently in each set
-    int testCount=0, validCount=0;
+    int testCount=0, validCount=0, trainCount=0;
+
+    // To randomize the data
+    int randNum;
+    bool added;
 
     while(input)
     {
         std::getline(input,line);
 
-        if(testCount != testNum)
-        {
-            outputTest << line << endl;
-            testCount++;
-        }
-        else if(validCount != validNum)
-        {
-            outputValid << line << endl;
-            validCount++;
-        }
-        else
-            outputTrain << line << endl;
+        // Skips blank lines
+        if(line.length() == 0)
+            continue;
 
+        randNum = rand()%10;
+        added = false;
+
+        switch(randNum)
+        {
+        // Training Dataset
+        case 0 ... 7:
+            addToFile(outputTrain,line,trainCount,trainNum,added);
+            addToFile(outputTest,line,testCount,testNum,added);
+            addToFile(outputValid,line,validCount,validNum,added);
+            break;
+        // Testing Dataset
+        case 8:
+            addToFile(outputTest,line,testCount,testNum,added);
+            addToFile(outputValid,line,validCount,validNum,added);
+            addToFile(outputTrain,line,trainCount,trainNum,added);
+            break;
+        // Validation Dataset
+        case 9:
+            addToFile(outputValid,line,validCount,validNum,added);
+            addToFile(outputTrain,line,trainCount,trainNum,added);
+            addToFile(outputTest,line,testCount,testNum,added);
+            break;
+        }
     }
+    /*
+    cout << trainCount << " " << trainNum << endl;
+    cout << testCount << " " << testNum << endl;
+    cout << validCount << " " << validNum << endl;
+    */
     input.close();
     outputTest.close();
     outputValid.close();
@@ -96,4 +126,20 @@ void checkValidStream(T &stream, string fileName)
         cerr << "Error: could not open " << fileName << "\n";
         exit(1);
     }
+}
+
+
+void addToFile(ofstream &stream, string line, int &curLines, int maxLines, bool &added)
+{
+    if(added == true)
+        return;
+
+    if(curLines != maxLines)
+    {
+        stream << line << endl;
+        curLines++;
+        added = true;
+    }
+    else
+        added = false;
 }
